618d14aa841cd10932c852db5398d7190dab836f
ar3d <info@ar3d.it>
2021-11-01 21:36:50 +0100

    ok

---
+++ data.js
@@ -0,0 +1,307 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-mura-ciclopiche-1",
+      "name": "Mura ciclopiche 1",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "yaw": 0.963492308798287,
+        "pitch": 0.058512746489780554,
+        "fov": 1.2618400982144773
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.2155707526909652,
+          "pitch": 0.11083597030358305,
+          "rotation": 0,
+          "target": "1-mura-ciclopiche-2"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-mura-ciclopiche-2",
+      "name": "Mura ciclopiche 2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "yaw": 2.8121172335704525,
+        "pitch": 0.16029053425133455,
+        "fov": 1.2618400982144773
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.9673631745148352,
+          "pitch": 0.21077380883381558,
+          "rotation": 0,
+          "target": "2-mura-ciclopiche-3"
+        },
+        {
+          "yaw": 0.012195882003046776,
+          "pitch": 0.02227920275429618,
+          "rotation": 0,
+          "target": "0-mura-ciclopiche-1"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-mura-ciclopiche-3",
+      "name": "Mura ciclopiche 3",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "yaw": 2.861480255710216,
+        "pitch": 0.0006436474603415121,
+        "fov": 1.2618400982144773
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.9496223916744677,
+          "pitch": 0.021177874157547905,
+          "rotation": 0,
+          "target": "3-mura-ciclopiche-4"
+        },
+        {
+          "yaw": 0.8805161152663352,
+          "pitch": 0.010281472337830166,
+          "rotation": 0,
+          "target": "1-mura-ciclopiche-2"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-mura-ciclopiche-4",
+      "name": "Mura ciclopiche 4",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "yaw": -3.0007401803151676,
+        "pitch": 0.14524909712242362,
+        "fov": 1.2618400982144773
+      },
+      "linkHotspots": [
+        {
+          "yaw": -3.07423976659458,
+          "pitch": 0.07154717478657169,
+          "rotation": 0,
+          "target": "4-mura-ciclopiche-5"
+        },
+        {
+          "yaw": 0.1687159028414129,
+          "pitch": 0.002051570614394649,
+          "rotation": 0,
+          "target": "2-mura-ciclopiche-3"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "4-mura-ciclopiche-5",
+      "name": "Mura ciclopiche 5",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "yaw": 3.095638108518804,
+        "pitch": -0.022547373894242995,
+        "fov": 1.2618400982144773
+      },
+      "linkHotspots": [
+        {
+          "yaw": -3.128788255883345,
+          "pitch": 0.05379785791489766,
+          "rotation": 0,
+          "target": "5-mura-ciclopiche-6"
+        },
+        {
+          "yaw": 0.18796001403840457,
+          "pitch": 0.04505753419620717,
+          "rotation": 0,
+          "target": "3-mura-ciclopiche-4"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "5-mura-ciclopiche-6",
+      "name": "Mura ciclopiche 6",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "yaw": 2.8270018035317976,
+        "pitch": 0.1627018799877895,
+        "fov": 1.2618400982144773
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.733343613134638,
+          "pitch": 0.05970448934102457,
+          "rotation": 0,
+          "target": "6-mura-ciclopiche-7"
+        },
+        {
+          "yaw": -0.08768348591761033,
+          "pitch": -0.006192829586725068,
+          "rotation": 0,
+          "target": "4-mura-ciclopiche-5"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "6-mura-ciclopiche-7",
+      "name": "Mura ciclopiche 7",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "yaw": -2.9259874050378336,
+        "pitch": 0.07632097368231427,
+        "fov": 1.2618400982144773
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.3062833063296715,
+          "pitch": 0.1061249204759136,
+          "rotation": 0,
+          "target": "5-mura-ciclopiche-6"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "qtvr",
+    "autorotateEnabled": true,
+    "fullscreenButton": true,
+    "viewControlButtons": false
+  }
+};
